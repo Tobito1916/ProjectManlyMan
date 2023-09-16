ff207455d69ed4da19560225f9675ba906630dbb
Tobito1916 <32836732+Tobito1916@users.noreply.github.com>
2023-09-16 17:01:13 +0200

    animationplayer added
    animationplayer + walk right animation added.
    
    resources for idle, walk right and walk left added

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -5,15 +5,29 @@ public partial class Movement : CharacterBody2D
 	[Export]
 	public int Speed { get; set; } = 400;
 
+	private AnimationPlayer _animationPlayer;
+
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
 	public void GetInput()
 	{
 
 		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
 		Velocity = inputDirection * Speed;
+		if (Input.IsActionPressed("right"))
+		{
+			_animationPlayer.Play("walk_right");
+		}
+		else
+		{
+			_animationPlayer.Stop();
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
-	{
+	{	
 		GetInput();
 		MoveAndSlide();
 	}
